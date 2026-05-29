635e2e5c98aeb1c43dcb39ddc4e3af3f995854bf
hoge1e3 <hoge1e3@gmail.com>
2026-05-29 10:49:08 +0900

    fix indent

--- sfile/test/test.ts
+++ sfile/test/test.ts
@@ -22,61 +22,61 @@ export async function getNodeFS():Promise<FileSystemFactory> {
 }
 
 export async function main(){
-let pass:number=0;
-//let testf: SFile;
-const cleanups=[] as (()=>Promise<any>)[];
-try {
-    const FS=await getNodeFS();
-    _console.log("metaurl",import.meta.url);
-    console.log(FS.get("/"));
-    console.log(FS.get("/").up());
-    const here=FS.get(import.meta.url);
-    const fixtureInSameDir=here.sibling("fixture/");
-    const fixture=fixtureInSameDir.exists() ? fixtureInSameDir : here.sibling("../../test/fixture/");
-    //const fixture=topDir;//.setPolicy({topDir});
-    await checkCopyDir(fixture);
-    //let cd =fixture;
-    const r=fixture.rel.bind(fixture);
-    const romd=r("rom/");
-    checkPathMethods(FS, fixture);
-    let ABCD = "abcd\nefg";
-    let CDEF = "defg\nてすと";
-    //obsolate: ls does not enum mounted dirs
-    //assert(r.indexOf("rom/")>=0, r);
-    //let romd = root.rel("rom/");
-    let ramd = fixture.rel("ram/");
-    if(ramd.exists()) await retryRmdir(ramd);
-    ramd.mkdir();
-    const testf = fixture.rel("testfn.txt");
-    cleanups.push(async ()=>testf.exists() && testf.rm());  
-    if (!testf.exists()) {
-        pass=1;
-        _console.log("Test #", pass);
-        await runPass1({fixture, romd, ramd, testf, ABCD, CDEF, cleanups});
-    } else {
-        pass=2;
-        _console.log("Test #", pass);
-        await runPass2({FS, fixture, romd, testf, ABCD});
-    }
-    if(ramd.exists()) await retryRmdir(ramd);
-    console.log("passed", "#"+pass);
-    if (pass==1) {
-        await timeout(1000);
-        if (typeof location!=="undefined" && !location.href.match(/pass1only/)) location.reload();
-    } else {
-        console.log("All test passed.");
-    }
-} catch (e) {
-    process.exitCode = 1;
-    console.log((e as any).stack);
-    alert("#"+pass+" test Failed. "+e);
+    let pass:number=0;
+    //let testf: SFile;
+    const cleanups=[] as (()=>Promise<any>)[];
     try {
-        for (let c of cleanups) await c();
+        const FS=await getNodeFS();
+        _console.log("metaurl",import.meta.url);
+        console.log(FS.get("/"));
+        console.log(FS.get("/").up());
+        const here=FS.get(import.meta.url);
+        const fixtureInSameDir=here.sibling("fixture/");
+        const fixture=fixtureInSameDir.exists() ? fixtureInSameDir : here.sibling("../../test/fixture/");
+        //const fixture=topDir;//.setPolicy({topDir});
+        await checkCopyDir(fixture);
+        //let cd =fixture;
+        const r=fixture.rel.bind(fixture);
+        const romd=r("rom/");
+        checkPathMethods(FS, fixture);
+        let ABCD = "abcd\nefg";
+        let CDEF = "defg\nてすと";
+        //obsolate: ls does not enum mounted dirs
+        //assert(r.indexOf("rom/")>=0, r);
+        //let romd = root.rel("rom/");
+        let ramd = fixture.rel("ram/");
+        if(ramd.exists()) await retryRmdir(ramd);
+        ramd.mkdir();
+        const testf = fixture.rel("testfn.txt");
+        cleanups.push(async ()=>testf.exists() && testf.rm());  
+        if (!testf.exists()) {
+            pass=1;
+            _console.log("Test #", pass);
+            await runPass1({fixture, romd, ramd, testf, ABCD, CDEF, cleanups});
+        } else {
+            pass=2;
+            _console.log("Test #", pass);
+            await runPass2({FS, fixture, romd, testf, ABCD});
+        }
+        if(ramd.exists()) await retryRmdir(ramd);
+        console.log("passed", "#"+pass);
+        if (pass==1) {
+            await timeout(1000);
+            if (typeof location!=="undefined" && !location.href.match(/pass1only/)) location.reload();
+        } else {
+            console.log("All test passed.");
+        }
     } catch (e) {
-        _console.error(e);
+        process.exitCode = 1;
+        console.log((e as any).stack);
+        alert("#"+pass+" test Failed. "+e);
+        try {
+            for (let c of cleanups) await c();
+        } catch (e) {
+            _console.error(e);
+        }
+        console.log("Unknown tags:", JSON.stringify(_console.unknownlist,null,2))
     }
-    console.log("Unknown tags:", JSON.stringify(_console.unknownlist,null,2))
-}
 }
 // of main()
 //(globalThis as any).main=main;
