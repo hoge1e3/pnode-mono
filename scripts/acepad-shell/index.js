98b397baa1c8f7b559fabfdb40a60918282d739a
hoge1e3 <hoge1e3@gmail.com>
2026-08-07 17:00:29 +0900

    apsh cmd_in_APSH_PATH

--- scripts/acepad-shell/index.js
+++ scripts/acepad-shell/index.js
@@ -1,12 +1,29 @@
 
 import * as process from "node:process";
 import * as path from "node:path";
+import * as fs from "node:fs";
 import FS from "@hoge1e3/sfile-node";
 
 import { getHome, Shell } from "./shell.mjs";
 //console.log(process.argv);
 const script=process.argv[2];
-const scriptFull=path.join(process.cwd(),script);
+if(!script) {
+    console.error("Usage: apsh script args...");
+    process.exit(1);
+}
+let scriptFull=path.join(process.cwd(),script);
+if (!fs.existsSync(scriptFull)) {
+    if (process.env.APSH_PATH) {
+        for (let p of process.env.APSH_PATH.split(path.delimiter)) {
+            scriptFull=path.join(p,script);
+            if (fs.existsSync(scriptFull)) break;
+        }
+    }
+    if (!fs.existsSync(scriptFull)) {
+        console.error(`${scriptFull}: No such file.`);
+        process.exit(1);
+    }
+}
 const scriptURL="file://"+scriptFull.replace(/\\/g,"/");
 //console.log(scriptURL);
 const {main}=await import(scriptURL);
